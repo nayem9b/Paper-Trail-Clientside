b7c0de78222de03dd487d66845c215804e281e0d
nayem9b <razwanniam1@gmail.com>
2023-07-14 09:12:54 +0600

    userSlice created

---
+++ src/redux/features/user/user.ts
@@ -0,0 +1,76 @@
+/* eslint-disable @typescript-eslint/no-unsafe-member-access */
+/* eslint-disable @typescript-eslint/no-unsafe-return */
+/* eslint-disable @typescript-eslint/no-unsafe-call */
+/* eslint-disable @typescript-eslint/no-unsafe-assignment */
+import { PayloadAction, createAsyncThunk, createSlice } from "@reduxjs/toolkit";
+import {
+  createUserWithEmailAndPassword,
+  signInWithEmailAndPassword,
+} from "firebase/auth";
+import { auth } from "../../../Firebase/firebase";
+
+interface IUserState {
+  user: {
+    email: string | null;
+  };
+  isLoading: boolean;
+  isError: boolean;
+  error: string | null;
+}
+
+interface Icredentials {
+  email: string;
+  password: string;
+}
+
+const initialState: IUserState = {
+  user: {
+    email: null,
+  },
+  isLoading: false,
+  isError: false,
+  error: null,
+};
+
+export const createUser = createAsyncThunk(
+  "user/createUser",
+  async ({ email, password }: Icredentials) => {
+    const data = await createUserWithEmailAndPassword(auth, email, password);
+    console.log(auth, data);
+    return data.user.email;
+  }
+);
+
+const userSlice = createSlice({
+  name: "user",
+  initialState,
+  reducers: {
+    setUser: (state, action: PayloadAction<string | null>) => {
+      state.user.email = action.payload;
+    },
+    setLoading: (state, action: PayloadAction<boolean>) => {
+      state.isLoading = action.payload;
+    },
+  },
+  extraReducers: (builder) => {
+    builder
+      .addCase(createUser.pending, (state) => {
+        state.isLoading = true;
+        state.isError = false;
+        state.error = null;
+      })
+      .addCase(createUser.fulfilled, (state, action) => {
+        state.user.email = action.payload;
+        state.isLoading = false;
+      })
+      .addCase(createUser.rejected, (state, action) => {
+        state.user.email = null;
+        state.isError = true;
+        state.error = action.error.message as string;
+        state.isLoading = false;
+      });
+  },
+});
+
+export const { setUser, setLoading } = userSlice.actions;
+export default userSlice.reducer;
