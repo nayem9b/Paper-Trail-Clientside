ae23047b6ceab0f14a3bfc4f9dce78adf09a0e72
nayem9b <razwanniam1@gmail.com>
2023-07-14 09:30:40 +0600

    login form added

--- src/routes/Route.tsx
+++ src/routes/Route.tsx
@@ -1,6 +1,7 @@
 import { createBrowserRouter } from "react-router-dom";
 import Root from "../Root/Root";
 import LandingPage from "../Components/Home/LandingPage";
+import SignUpForm from "../Components/Auth/SignUpForm";
 
 export const router = createBrowserRouter([
   {
@@ -10,6 +11,10 @@ export const router = createBrowserRouter([
       {
         path: "/",
         element: <LandingPage></LandingPage>,
+      },
+      {
+        path: "/signUp",
+        element: <SignUpForm></SignUpForm>,
       },
     ],
   },
